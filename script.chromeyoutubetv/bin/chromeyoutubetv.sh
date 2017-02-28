09d1ad0963b471a939b03b7c28b190194f4aaf75
Timo Verbrugghe <timo@hotmail.be>
2017-02-28 13:21:29 +0100

    Change export PATH

--- script.chromeyoutubetv/bin/chromeyoutubetv.sh
+++ script.chromeyoutubetv/bin/chromeyoutubetv.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 killall chrome || true
 /storage/data/ubuntu/usr/sbin/chroot /storage/data/ubuntu /bin/bash -x << EOF
+export PATH=$PATH:/bin:/usr/local/sbin:/usr/sbin:/sbin
 chmod 1777 /dev/shm
 su htpc
 unclutter -idle 0.01 -root;
