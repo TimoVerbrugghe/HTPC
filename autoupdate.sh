dbeafcf0407a63533abb83cd8139b826c519a9e3
Timo Verbrugghe <timo@hotmail.be>
2017-04-13 14:16:22 +0200

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -19,7 +19,7 @@ pacman -Syuq --noconfirm >> $UPDATE_LOG 2>&1
 
 # Getting return code from pacman. If this return code is not 0 (so an error has occured with the pacman update), notify system administrator
 errorval="$?"
-if [ $retval -ne 0 ]; then
+if [ $errorval -ne 0 ]; then
   $PUSHBULLET_SCRIPT "ERROR - HTPC Auto update failed" "During auto update, the pacman installer failed installing updates (error code different than 0)." >/dev/null 2>&1
 	exit 1
 else
