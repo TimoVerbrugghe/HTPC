6aa222c7b51f79cbb799f5d130d415dd9aa1a642
Timo Verbrugghe <timo@hotmail.be>
2018-03-12 00:17:47 +0100

    Change of parsec pc id

--- addons/script.parsec/bin/parsec.sh
+++ addons/script.parsec/bin/parsec.sh
@@ -19,6 +19,6 @@ printf "Wait half a second before opening Parsec" > $logfile 2>&1
 sleep 0.5
 
 printf "Opening Parsec" > $logfile 2>&1
-DISPLAY=:0 /usr/bin/parsec server_id=48191:client_overlay=0:app_lan_quality=2:app_wan_quality:2 > $logfile 2>&1
+DISPLAY=:0 /usr/bin/parsec server_id=116766:client_overlay=0:app_lan_quality=2:app_wan_quality:2 > $logfile 2>&1
 
 exit 0
