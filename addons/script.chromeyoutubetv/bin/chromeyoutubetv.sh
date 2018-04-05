406b6b8922c556045172c2e279a003aa10cf9f01
Timo Verbrugghe <timo@hotmail.be>
2018-04-05 13:52:03 +0200

    Disable kodi when youtube is running
    Otherwise Kodi's suspend timer will keep running even while watching
    youtube

--- addons/script.chromeyoutubetv/bin/chromeyoutubetv.sh
+++ addons/script.chromeyoutubetv/bin/chromeyoutubetv.sh
@@ -15,13 +15,18 @@ killall chromium || true > $logfile 2>&1
 printf "Put Kodi window in the background" > $logfile 2>&1
 DISPLAY=:0 wmctrl -r kodi -b add,hidden
 
-# printf "Freeze/stop kodi from running" > $logfile 2>&1
-# killall -STOP kodi.bin
+printf "Freeze/stop kodi from running" > $logfile 2>&1
+# kill only accepts PIDs, so find kodi's pid with pgrep, then apply kill -STOP on it
+pgrep -f kodi.bin | xargs kill -STOP
 
 printf "Wait half a second before opening Google Chrome" > $logfile 2>&1
 sleep 0.5
 
 printf "Opening Chromium" > $logfile 2>&1
 DISPLAY=:0 /usr/bin/chromium https://www.youtube.com/tv/ --kiosk --noerrdialogs > $logfile 2>&1
+
+printf "Resume Kodi" > $logfile 2>&1
+# kill only accepts PIDs, so find kodi's pid with pgrep, then apply kill -STOP on it
+pgrep -f kodi.bin | xargs kill -CONT
 
 exit 0
